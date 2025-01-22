03c635cf58d7f13b710cfcd23298f738646d8680
Chinito <clark.wayne023@gmail.com>
2025-01-22 15:41:06 +0800

    Fix Lint Errors

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -7,6 +7,7 @@ import Link from "next/link";
 import { AnimatePresence, motion } from "framer-motion";
 import { useOutsideClick } from "@/hooks/use-outside-click";
 import { useEffect, useId, useRef, useState } from "react";
+import { CloseIcon } from "@/components/CloseIcon";
 
 export default function Projects() {
   const [active, setActive] = useState<
@@ -215,36 +216,3 @@ export default function Projects() {
     </motion.div>
   );
 }
-
-export const CloseIcon = () => {
-  return (
-    <motion.svg
-      initial={{
-        opacity: 0,
-      }}
-      animate={{
-        opacity: 1,
-      }}
-      exit={{
-        opacity: 0,
-        transition: {
-          duration: 0.05,
-        },
-      }}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className="h-4 w-4 text-black"
-    >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-      <path d="M18 6l-12 12" />
-      <path d="M6 6l12 12" />
-    </motion.svg>
-  );
-};
